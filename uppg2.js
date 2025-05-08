bebb949018d3daf8a27f4cd32665879762079b9e
000henrik@gmail.com <000henrik@gmail.com>
2025-05-08 10:56:45 +0200

    Done uppg 1-3

--- uppg2.js
+++ uppg2.js
@@ -4,6 +4,8 @@ function uppg2() {
    // deklarera en variable med ditt namn och skriv ut det i konsolen
 
 
+   let name = "Henrik Martini";
+   console.log(name); 
 }
 
 module.exports = { uppg2 };
